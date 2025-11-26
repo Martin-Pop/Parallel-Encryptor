b0a673927a92e892a3f58f06f59b1a18544fa340
Martin <martinpop06@gmail.com>
2025-11-26 15:45:19 +0100

    added chunk reader

---
+++ io_utils/reader.py
@@ -0,0 +1,18 @@
+def chunk_reader(file_name, chunk_size, chunk_queue):
+
+    try:
+        with open(file_name, "rb") as f:
+            chunk_id = 0
+            while True:
+                chunk = f.read(chunk_size)
+                if not chunk:
+                    break
+                # print(chunk)
+                chunk_queue.put((chunk_id, chunk))
+                chunk_id += 1
+
+        chunk_queue.put(None)
+    except FileNotFoundError:
+       print('File not found')
+    except Exception as e:
+        print('Unexpected error: {}'.format(e))
--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+from multiprocessing import Queue, Process
+
+from io_utils.reader import chunk_reader
 
 def main():
-    pass
+
+    file_path = 'test.txt'
+    chunk_size = 1024
+    chunk_queue = Queue()
+
+    reader = Process(target= chunk_reader, args=(file_path, chunk_size, chunk_queue))
+    reader.start()
+
+    reader.join()
+    print('Done')
+    print(chunk_queue)
 
 
 if __name__ == "__main__":
